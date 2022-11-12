211c5e5f4a5a1abbfeacdfa1ad62682d12a13658
Christophe JEAN <54830918+chris13300@users.noreply.github.com>
2022-11-12 17:23:48 +0100

    Update modMain.vb

--- expExtract/modMain.vb
+++ expExtract/modMain.vb
@@ -18,24 +18,10 @@
         Dim fichierPGN As String, fichierINI As String, pgnExtract As String, fichierUCI As String, priorite As Integer
         Dim chaine As String, tabChaine() As String, tabTmp() As String, i As Integer
 
-        If My.Computer.FileSystem.GetFileInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) & "\Documents\Visual Studio 2013\Projects\expExtract\expExtract\bin\Debug\expExtract.exe").LastWriteTime > My.Computer.FileSystem.GetFileInfo(My.Application.Info.AssemblyName & ".exe").LastWriteTime Then
-            MsgBox("Il existe une version plus récente de ce programme !", MsgBoxStyle.Information)
-            End
-        End If
-
         'chargement parametres
-        moteurEXP = "D:\JEUX\ARENA CHESS 3.5.1\Engines\Eman\20T Eman 8.20 x64 PCNT.exe"
-        sourceEXP = "D:\JEUX\ARENA CHESS 3.5.1\Engines\Eman\Eman.exp"
-        pgnExtract = "D:\JEUX\ARENA CHESS 3.5.1\Databases\PGN Extract GUI\pgn-extract.exe"
-        If My.Computer.Name = "BOIS" Or My.Computer.Name = "HTPC" Or My.Computer.Name = "TOUR-COURTOISIE" Then
-            moteurEXP = "D:\JEUX\ARENA CHESS 3.5.1\Engines\Eman\20T Eman 8.20 x64 BMI2.exe"
-            sourceEXP = "D:\JEUX\ARENA CHESS 3.5.1\Engines\Eman\Eman.exp"
-            pgnExtract = "D:\JEUX\ARENA CHESS 3.5.1\Databases\PGN Extract GUI\pgn-extract.exe"
-        ElseIf My.Computer.Name = "BUREAU" Or My.Computer.Name = "WORKSTATION" Then
-            moteurEXP = "E:\JEUX\ARENA CHESS 3.5.1\Engines\Eman\20T Eman 8.20 x64 BMI2.exe"
-            sourceEXP = "E:\JEUX\ARENA CHESS 3.5.1\Engines\Eman\Eman.exp"
-            pgnExtract = "E:\JEUX\ARENA CHESS 3.5.1\Databases\PGN Extract GUI\pgn-extract.exe"
-        End If
+        moteurEXP = "Eman.exe"
+        sourceEXP = "Eman.exp"
+        pgnExtract = "pgn-extract.exe"
         priorite = 64
         fichierINI = My.Computer.Name & ".ini"
         If My.Computer.FileSystem.FileExists(fichierINI) Then
